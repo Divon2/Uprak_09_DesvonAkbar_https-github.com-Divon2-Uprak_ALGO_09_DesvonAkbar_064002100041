88678756757f38982074d8d0d1663d287e1c4cbe
Divon2 <91172013+Divon2@users.noreply.github.com>
2021-11-24 09:52:58 +0700

    Add files via upload

---
+++ elkom 09 tugas 3 desvon.py
@@ -0,0 +1,7 @@
+my_list = [1, 2, 3, 4, 5]
+my_new_list = [i * 2 for i in my_list]
+my_new_list = [i * 3 for i in my_list] 
+my_new_list = [i * 4 for i in my_list] 
+my_new_list = [i * 5 for i in my_list] 
+print(my_new_list) 
+
